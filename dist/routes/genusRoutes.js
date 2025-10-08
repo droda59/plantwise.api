2245d11fdbeb81ce7e9f1128bcf402b6eb1d1672
droda59 <swcommander@gmail.com>
2025-10-08 12:22:04 -0400

    Weird that dist was included

--- dist/routes/genusRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const genusController_1 = require("../controllers/genusController");
-const router = (0, express_1.Router)();
-router.get('/', genusController_1.getGenusList);
-router.get('/:id', genusController_1.getSpeciesForGenus);
-exports.default = router;
--- dist/routes/speciesRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const speciesController_1 = require("../controllers/speciesController");
-const router = (0, express_1.Router)();
-router.get('/', speciesController_1.getPlantsBySpecies);
-router.get('/all', speciesController_1.getAllSpecies);
-exports.default = router;
